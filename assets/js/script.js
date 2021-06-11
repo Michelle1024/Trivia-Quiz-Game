4e702c07cb378078fe4f971e1efc626d4e8d7234
Michelle1024 <michelleldqp@yahoo.com>
2021-06-11 13:18:33 +0000

    edit css and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,7 +32,7 @@ Question.prototype.isCorrectAnswer = function(choice) {
 
 // Display the questions
 function displayQuestion() {
-    if (Quiz.isEnded()) {
+    if (quiz.isEnded()) {
         showScores();
     }
     else {
@@ -104,3 +104,31 @@ let questions = [
     )
 ];
 
+// Create quiz
+let quiz = new Quiz(questions);
+
+// Display quiz
+displayQuestion();
+
+//Add a Timer
+let time = 2;
+let quizTimeInMinutes = time * 60 * 60;
+let quizTime = quizTimeInMinutes / 60;
+
+let counting = document.getElementById("count-down");
+
+function startCountdown() {
+    let quizTimer = setInterval(function() {
+        if (quizTime <= 0) {
+            clearInterval(quizTimer);
+            showScores();
+        } else {
+            quizTime--;
+            let sec = Math.floor(quizTime % 60);
+            let min = Math.floor(quizTime / 60) % 60;
+            counting.innerHTML = `TIME: ${min} : ${sec}`;
+        }
+    }, 1000);
+}
+
+startCountdown();
